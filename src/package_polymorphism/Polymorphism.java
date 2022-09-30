e252c2a6c5b64101c8cbc10035a86d8ab340ee7c
Abhishek K <kabhik2898@gmail.com>
2022-09-30 10:49:05 +0530

    First commit Polymorphism class added

---
+++ src/package_polymorphism/Polymorphism.java
@@ -0,0 +1,16 @@
+package package_polymorphism;
+
+public class Polymorphism {
+
+	public int addition(int a , int b) {
+		return a+b;
+	}
+	
+	public double addition(double a, double b, double c) {
+		return a+b+c;
+	}
+	
+	public double addition(int a, float b, double c) {
+		return a+b+c;
+	}
+}
